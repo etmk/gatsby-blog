a660acb3bb5093a020293c0125e2f001e8b0af88
sunjae-kim <kimsj9484@gmail.com>
2019-04-10 21:50:58 +0900

    Create next and prev posting card
     - Create renderCard method
     - Change graphql query to get all md files in oreder to get next and prev posting
     - Separate styled tags

--- src/templates/blog-post.jsx
+++ src/templates/blog-post.jsx
@@ -1,37 +1,17 @@
 import React, { Component } from 'react';
-import { graphql } from 'gatsby';
-import styled from '@emotion/styled';
+import { graphql, Link } from 'gatsby';
 import PropTypes from 'prop-types';
-import { Container, Divider } from 'semantic-ui-react';
+import { Container, Divider, Icon } from 'semantic-ui-react';
 import SEO from '../components/seo';
 import store from '../utils/store';
 import Layout from '../components/layout';
 import { TOGGLE_IS_IN_DOCUMENT } from '../modules/actionTypes';
+import {
+  BlogPostBox, MarkerHeader, HashTagBox, HashTag,
+  Content, MarkedHeader, HeaderDate, MarkDown,
+} from './styledTags';
 
-const Content = styled.div`
-  margin-top: 1.5em;
-  text-align: start;
-  display: flex;
-  flex-flow: column;
-  min-height: 68vh;
-`;
-
-const MarkedHeader = styled.h1`
-  display: inline;
-  border-radius: 1em 0 1em 0;
-`;
-
-const HeaderDate = styled.h3`
-  margin-top: 10px;
-  margin-bottom: 1.5em;
-  color: #bbb;
-  font-size: 20px;
-`;
-
-const MarkDown = styled.div`
-  font-size: larger;
-`;
-
+/* global window */
 class Template extends Component {
   componentDidMount = () => {
     store.dispatch({ type: TOGGLE_IS_IN_DOCUMENT });
@@ -41,36 +21,103 @@ class Template extends Component {
     store.dispatch({ type: TOGGLE_IS_IN_DOCUMENT });
   }
 
-  render = () => {
+  renderCard = (post, isPrev) => (
+    <BlogPostBox>
+      <Link
+        key={post.id}
+        to={post.frontmatter.path}
+      >
+        <div className="blog-post">
+          <div style={{ display: 'flex' }}>
+            <div style={{
+              display: 'flex', flex: 1, justifyContent: 'flex-start', alignItems: 'center',
+            }}
+            >
+              {isPrev && <Icon name="angle left" size="large" />}
+            </div>
+            <div style={{ flex: 15 }}>
+              <MarkerHeader>
+                {post.frontmatter.title}
+              </MarkerHeader>
+              <HashTagBox>
+                {post.frontmatter.tags.map(tag => <HashTag key={tag}>{tag}</HashTag>)}
+              </HashTagBox>
+            </div>
+            <div style={{
+              display: 'flex', flex: 1, justifyContent: 'flex-end', alignItems: 'center',
+            }}
+            >
+              {!isPrev && <Icon name="angle right" size="large" />}
+            </div>
+          </div>
+        </div>
+      </Link>
+    </BlogPostBox>
+  )
+
+  renderPost = ({ prevPost, post, nextPost }) => (
+    <>
+      <SEO title="Posting" keywords={post.frontmatter.tags} />
+      <Container>
+        <Content>
+          <MarkedHeader>{post.frontmatter.title}</MarkedHeader>
+          <HeaderDate>
+            {post.frontmatter.date}
+          </HeaderDate>
+          <Divider />
+          <MarkDown dangerouslySetInnerHTML={{ __html: post.html }} />
+          <div style={{ marginTop: '30px' }}>
+            {prevPost && this.renderCard(prevPost, true)}
+            {nextPost && this.renderCard(nextPost, false)}
+          </div>
+        </Content>
+      </Container>
+    </>
+  );
+
+  getPostings = () => {
     const { data } = this.props;
-    const post = data.markdownRemark;
+    const { pathname } = window.location;
+    const endpoint = pathname.split('/').pop();
+    const currentPage = pathname.split('/')[1];
+    const postings = data.allMarkdownRemark.edges.filter(
+      ele => ele.node.frontmatter.category === currentPage,
+    );
+    return postings.reduce((acc, posting, index, self) => {
+      if (endpoint === posting.node.frontmatter.path.split('/').pop()) {
+        acc.post = self[index].node;
+        if (index !== 0) acc.nextPost = self[index - 1].node;
+        if (index !== postings.length - 1) acc.prevPost = self[index + 1].node;
+      }
+      return acc;
+    }, {});
+  }
+
+  render = () => {
+    const { prevPost, post, nextPost } = this.getPostings();
     return (
       <Layout>
-        <SEO title="Posting" keywords={post.frontmatter.tags} />
-        <Container>
-          <Content>
-            <MarkedHeader>{post.frontmatter.title}</MarkedHeader>
-            <HeaderDate>
-              {post.frontmatter.date}
-            </HeaderDate>
-            <Divider />
-            <MarkDown dangerouslySetInnerHTML={{ __html: post.html }} />
-          </Content>
-        </Container>
+        {this.renderPost({ prevPost, post, nextPost })}
       </Layout>
     );
   };
 }
 
 export const pageQuery = graphql`
-  query($path: String!) {
-    markdownRemark(frontmatter: { path: { eq: $path } }) {
-      html
-      frontmatter {
-        date(formatString: "DD MMMM, YYYY")
-        path
-        title
-        category
+  query {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          html
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+            path
+            category
+            tags
+          }
+        }
       }
     }
   }
---
+++ src/templates/styledTags.js
@@ -0,0 +1,42 @@
+import styled from '@emotion/styled';
+
+export const Content = styled.div`
+  margin-top: 1.5em;
+  text-align: start;
+  display: flex;
+  flex-flow: column;
+  min-height: 68vh;
+`;
+
+export const MarkedHeader = styled.h1`
+  display: inline;
+  border-radius: 1em 0 1em 0;
+`;
+
+export const HeaderDate = styled.h3`
+  margin-top: 10px;
+  margin-bottom: 1.5em;
+  color: #bbb;
+  font-size: 20px;
+`;
+
+export const MarkDown = styled.div`
+  font-size: larger;
+`;
+
+export const BlogPostBox = styled.div`
+  margin-top: 30px;
+`;
+
+export const MarkerHeader = styled.h3`
+  margin-bottom: 0px;
+`;
+
+export const HashTagBox = styled.div`
+  margin-top: 7px;
+`;
+
+export const HashTag = styled.span`
+  color: #aaa;
+  margin-right: 8px;
+`;
